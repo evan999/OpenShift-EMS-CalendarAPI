d3e6754ec03d7694533faca8bdd0c4c7967393c1
evan999 <emeshberge@gmail.com>
2021-12-26 20:58:23 -0500

    Initialize Spring Boot project for Calendar API

---
+++ src/main/java/com/ems/CalendarClient/CalendarClientApplication.java
@@ -0,0 +1,13 @@
+package com.ems.CalendarClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ems/CalendarClient/CalendarClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ems.CalendarClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
